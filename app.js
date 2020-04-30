a1e785486e7e0e7466036b885e34c104b2649014
vd89 <me@daxcodes.in>
2020-04-30 13:04:57 +0530

    the final with requestion and then find for other city

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
 const readLineSync = require('readline-sync');
 const axios = require('axios');
+let qusOne = 'n';
 
 
-const weatherData = () => {
-    console.clear();
-    axios
-        .get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=ee4102e4815321c59c6bf45bd915082c`)
+async function weatherData(){
+    do {
+    let cName = readLineSync.question('Give the name of the city: ');
+   await axios
+    .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cName}&APPID=ee4102e4815321c59c6bf45bd915082c`,
+        )
         .then((res) => {
-            let data = res.data
-            let main = res.data.main
-            let cityTempMax = parseInt(main.temp_max - 273.15)
-            let cityTempMin = parseInt(main.temp_min - 273.15)
-            let cityHumidity = main.humidity
-            let cityName = data.name
-            let country = data.sys.country
+      let data = res.data;
+      let main = res.data.main;
+      let cityTempMax = parseInt(main.temp_max - 273.15);
+      let cityTempMin = parseInt(main.temp_min - 273.15);
+      let cityHumidity = main.humidity;
+      let cityName = data.name;
+      let country = data.sys.country;
 
-            let CityObj = {
-                cityName,
-                country,
-                cityTempMax,
-                cityTempMin,
-                cityHumidity
-            }
-            console.log('CityObj :>> ', CityObj);
+      let CityObj = {
+        cityName,
+        country,
+        cityTempMax,
+        cityTempMin,
+        cityHumidity,
+      };
+      console.log('CityObj :>> ', CityObj);
+      qusOne = readLineSync.question(`Do you want continue 'yes','no': `);
+    })
+    .catch((err) => {
+        throw err;
+    });
+} while (qusOne==='y');
+};
 
-        }).catch((err) => {
-            throw err
-        });
+ weatherData()
     
-}
 
-let cityName = readLineSync.question("Please provide the name of the City: ")
-weatherData()
+// console.log(qusOne);
