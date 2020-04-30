2f38e1a8cef4409a0e0a5f17504eef1d90361cfb
vd89 <me@daxcodes.in>
2020-04-30 17:14:08 +0530

    the destructure of the Objects

--- app.js
+++ app.js
@@ -1,46 +1,34 @@
 const readLineSync = require('readline-sync');
 const axios = require('axios');
-let qusOne = 'n';
 
-
-async function weatherData(){
-  
-    let cName = readLineSync.question('Give the name of the city: ');
-    axios
+function weatherData() {
+  let cName = readLineSync.question('Give the name of the city: ');
+  axios
     .get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${cName}&APPID=ee4102e4815321c59c6bf45bd915082c`,
-        )
-        .then((res) => {
-      let data = res.data;
-      let main = res.data.main;
-      let cityTempMax = parseInt(main.temp_max - 273.15) +' C';
-      let cityTempMin = parseInt(main.temp_min - 273.15) +' C';
-      let cityHumidity = main.humidity;
-      let cityName = data.name;
-      let country = data.sys.country;
-
+      `http://api.openweathermap.org/data/2.5/weather?q=${cName}&APPID=ee4102e4815321c59c6bf45bd915082c`,
+    )
+    .then((res) => {
+      const { main, name, sys } = res.data;
       let CityObj = {
-        cityName,
-        country,
-        cityTempMax,
-        cityTempMin,
-        cityHumidity,
+        cityName: name,
+        country: sys.country,
+        cityTempMax: parseInt(main.temp_max - 273.15) + ' C',
+        cityTempMin: parseInt(main.temp_min - 273.15) + ' C',
+        cityHumidity: main.humidity,
       };
       console.log('CityObj :>> ', CityObj);
-          qusOne = readLineSync.question(`Do you want continue 'yes','no': `);
-          if (qusOne==='y') {
-            weatherData()
-          } else {
-            console.log(`Thanks for using the :thermometer: :thumbsup:`);
-          }
+      let qusOne = readLineSync.question(`Do you want continue 'yes','no': `);
+      if (qusOne === 'y') {
+        weatherData();
+      } else {
+        console.log(`Thanks for using the :thermometer: :thumbsup:`);
+      }
     })
     .catch((err) => {
-        throw err;
+      throw err;
     });
+}
 
-};
-
- weatherData()
-    
+weatherData();
 
 // console.log(qusOne);
